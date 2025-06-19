25e2299487dc6abb2c7f1212af3bf8c68ee630c6
acerstructor <acerstructorofficial@gmail.com>
2025-06-19 21:05:54 +0800

    Included definitions.h header files

--- game/src/entry.c
+++ game/src/entry.c
@@ -1,6 +1,7 @@
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL_main.h>
 #include "structs.h"
+#include "definitions.h"
 
 ProgramData g_programData;
 
