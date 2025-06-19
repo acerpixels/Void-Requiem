d1ab9f09d060a60f09d55a26169e02c4764e43d8
acerstructor <acerstructorofficial@gmail.com>
2025-06-19 20:22:42 +0800

    Removed SDL.h library at entry.c

--- game/src/entry.c
+++ game/src/entry.c
@@ -1,5 +1,5 @@
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
-#include <SDL3/SDL.h>
+
 #include <SDL3/SDL_main.h>
 
 /* We will use this renderer to draw into this window every frame. */
