bc55d0ceb2802beaae2663bd7e5e47c78f690454
acerstructor <acerstructorofficial@gmail.com>
2025-06-19 16:11:54 +0800

    Applied SDL3 provided template to main.c

--- game/src/main.c
+++ game/src/main.c
@@ -1,7 +1,50 @@
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main(int argc, char** argv)
+/* We will use this renderer to draw into this window every frame. */
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-	SDL_Log("VOID REQUIEM\n");
-	return 0;
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+
+	if (!SDL_CreateWindowAndRenderer("Void Requiem", 640, 480, 0, &window, &renderer))
+	{
+		SDL_Log("Couldn't create window & renderer: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+	return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+	if (event->type == SDL_EVENT_QUIT)
+	{
+		return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+	}
+	return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+	/* SDL will clean up the window/renderer for us. */
+}
+
