a777b28fc07b448f5dc0a6920d158376e700e2a0
acerstructor <acerstructorofficial@gmail.com>
2025-06-20 21:26:41 +0800

    Added boolean definitions

--- game/src/definitions.h
+++ game/src/definitions.h
@@ -5,3 +5,9 @@
 #define GAME_WINDOW_TITLE   "Void Requiem"
 #define GAME_WINDOW_WIDTH   640
 #define GAME_WINDOW_HEIGHT  480
+
+typedef char BYTE;
+typedef BYTE BOOL;
+
+#define FALSE   0
+#define TRUE    1
