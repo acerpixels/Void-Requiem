b6a5abdc5f77659f2147ee86ac360362f3664466
acerstructor <acerstructorofficial@gmail.com>
2025-06-20 22:16:47 +0800

    Added render module

---
+++ game/src/render.c
@@ -0,0 +1,8 @@
+#include "render.h"
+
+void render(WindowData* windowData)
+{
+	SDL_SetRenderDrawColor(windowData->renderer, 0, 0, 0, 255);
+	SDL_RenderClear(windowData->renderer);
+	SDL_RenderPresent(windowData->renderer);
+}
