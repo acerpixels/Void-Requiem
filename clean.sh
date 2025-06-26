031d4576eb2033507a8e17481b053959469b5f49
acerstructor <acerstructorofficial@gmail.com>
2025-06-26 16:08:56 +0800

    modified: makes clean.sh remove the build directory once executed

--- clean.sh
+++ clean.sh
@@ -7,48 +7,16 @@ if [[ ! -d "build-linux" && ! -d "build-windows" ]]; then
 	exit 0
 fi
 
-case "$PARAMETER_INPUT" in
-	REMOVE_DIR)
-		echo -e " ---- Proceeding to remove build directories ---- \n"
-
-		if [[ -d "build-linux" ]]; then
-			rm -rf "build-linux"
-			echo "LOG: build-linux directory has been removed"
-		fi
-
-		if [[ -d "build-windows" ]]; then
-			rm -rf "build-windows"
-			echo "LOG: build-linux directory has been removed"
-		fi
-
-		echo -e " ---- REMOVAL SUCCESSFUL ---- \n"
-		exit 0
-		;;
-esac
-
 echo -e " ---- Proceeding to clean build directories ---- \n"
 
 if [[ -d "build-linux" ]]; then
-	if [[ ! -x $(command -v make) ]]; then
-		echo -e "Error Occured: Could not find make command...\nPlease ensure that you've installed base-devel package properly" >&2
-		exit 1
-	fi
-	cd build-linux/
-	make clean
-	cd ..
-	echo "LOG: build-linux/ directory has been cleaned!"
+	rm -rf "build-linux"
+	echo "LOG: build-linux directory has been removed"
 fi
 
-
 if [[ -d "build-windows" ]]; then
-	if [[ ! -x $(command -v x86_64-w64-mingw32-make) ]]; then
-		echo -e "Error Occured: Could not find x86_64-w64-mingw32-make command...\nPlease ensure that you've installed mingw-w64-make package properly" >&2
-		exit 1
-	fi
-	cd build-windows/
-	x86_64-w64-mingw32-make clean
-	cd ..
-	echo "LOG: build-linux/ directory has been cleaned!"
+	rm -rf "build-windows"
+	echo "LOG: build-linux directory has been removed"
 fi
 
 echo -e " ---- CLEANING SUCCESSFUL ---- \n"
