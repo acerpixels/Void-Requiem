0d7279cf0bacd65740da8be5138e6f55ee88379a
acerstructor <acerstructorofficial@gmail.com>
2025-06-26 15:38:29 +0800

    calls scene pointer function draw

--- game/src/draw.c
+++ game/src/draw.c
@@ -8,6 +8,7 @@ void draw(WindowData* windowData)
 	RGBA bgColor = g_gameData.scene.backgroundCol;
 	SDL_SetRenderDrawColor(windowData->renderer, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
 
+	g_gameData.scene.draw(windowData->renderer);
 
 	SDL_RenderClear(windowData->renderer);
 	SDL_RenderPresent(windowData->renderer);
