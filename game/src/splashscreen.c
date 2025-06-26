1691bf7f87ff844292f039d7f78201b0897c19aa
acerstructor <acerstructorofficial@gmail.com>
2025-06-26 14:59:00 +0800

    Additionals: implemented static functions prototypes for splashcreen scene

--- game/src/splashscreen.c
+++ game/src/splashscreen.c
@@ -3,6 +3,12 @@
 
 extern GameData g_gameData;
 
+static void init();
+static void input();
+static void update();
+static void draw();
+static void cleanup();
+
 void init_splashscreen()
 {
 	// TODO
