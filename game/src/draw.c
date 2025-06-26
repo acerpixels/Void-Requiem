c03948d0c8a4f9c39aab812d56970e374c575493
acerstructor <acerstructorofficial@gmail.com>
2025-06-26 15:39:37 +0800

    modified: rearranged by moving SDL_RenderClear up and moving draw ptr function down

--- game/src/draw.c
+++ game/src/draw.c
@@ -7,9 +7,9 @@ void draw(WindowData* windowData)
 {
 	RGBA bgColor = g_gameData.scene.backgroundCol;
 	SDL_SetRenderDrawColor(windowData->renderer, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
+	SDL_RenderClear(windowData->renderer);
 
 	g_gameData.scene.draw(windowData->renderer);
 
-	SDL_RenderClear(windowData->renderer);
 	SDL_RenderPresent(windowData->renderer);
 }
