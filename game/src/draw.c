a145b86dac16c2c3eb11afd2ccebe90c48825074
acerstructor <acerstructorofficial@gmail.com>
2025-06-26 15:37:07 +0800

    misc: added spaces

--- game/src/draw.c
+++ game/src/draw.c
@@ -7,6 +7,8 @@ void draw(WindowData* windowData)
 {
 	RGBA bgColor = g_gameData.scene.backgroundCol;
 	SDL_SetRenderDrawColor(windowData->renderer, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
+
+
 	SDL_RenderClear(windowData->renderer);
 	SDL_RenderPresent(windowData->renderer);
 }
