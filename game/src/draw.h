8d96207916703b422026c8a63739ddeff1b7ddb1
acerstructor <acerstructorofficial@gmail.com>
2025-07-03 10:28:35 +0800

    moved draw function to init module

--- game/src/draw.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "headers.h"
-#include "structs.h"
-#include "init.h"
-#include "definitions.h"
-
-void draw(__window_data* windowData);
--- game/src/init.c
+++ game/src/init.c
@@ -23,3 +23,14 @@ bool __init_windowData(__window_data* windowData)
 	}
 	return true;
 }
+
+void __draw(__window_data* windowData, GameData gameData)
+{
+	SDL_Color bgColor = gameData.scene.backgroundCol;
+	SDL_SetRenderDrawColor(windowData->renderer, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
+	SDL_RenderClear(windowData->renderer);
+
+	gameData.scene.draw(windowData->renderer);
+
+	SDL_RenderPresent(windowData->renderer);
+}
