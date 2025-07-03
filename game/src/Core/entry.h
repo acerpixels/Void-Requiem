acee87aa2685dc31237d0f7f137d5940c8782912
acerstructor <acerstructorofficial@gmail.com>
2025-07-03 13:22:52 +0800

    moved contents from entry.c to entry.h

---
+++ game/src/Core/entry.h
@@ -0,0 +1,50 @@
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#include <SDL3/SDL_main.h>
+
+#include "window_manage.h"
+#include "../splashscreen.h"
+
+/* GLOBAL VARIABLE */
+GameData g_gameData;
+
+/* LOCAL VARIABLE */
+static __window_data s_windowData;
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+	if (!__init_windowData(&s_windowData))
+		return SDL_APP_FAILURE;
+	
+	init_splashscreen();
+	SDL_assert(g_gameData.scene.init != NULL);
+	
+	return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+	if (event->type == SDL_EVENT_QUIT)
+		return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+
+	g_gameData.scene.input(event);
+
+	return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+	g_gameData.scene.update();
+	__draw(&s_windowData, &g_gameData);
+	return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+	g_gameData.scene.cleanup();
+	/* SDL will clean up the window/renderer for us. */
+}
+
