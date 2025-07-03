76bd87b7029cb87e2cfe03b59399171dddfae8ba
acerstructor <acerstructorofficial@gmail.com>
2025-07-03 10:37:39 +0800

    changes: renamed draw to __draw

--- game/src/entry.c
+++ game/src/entry.c
@@ -2,7 +2,6 @@
 #include <SDL3/SDL_main.h>
 
 #include "init.h"
-#include "draw.h"
 #include "splashscreen.h"
 
 /* GLOBAL VARIABLE */
@@ -38,7 +37,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
 	g_gameData.scene.update();
-	draw(&s_windowData);
+	__draw(&s_windowData, &g_gameData);
 	return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
