ee952acfe0b9062b51cb3883f8517a380d561f71
acerstructor <acerstructorofficial@gmail.com>
2025-07-04 14:24:51 +0800

    changes: moved contents from structs.h to Core/applications.h

--- game/src/Core/application.h
+++ game/src/Core/application.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <SDL3/SDL_render.h>
 
 typedef struct __app_config
 {
@@ -8,5 +9,23 @@ typedef struct __app_config
 	char app_title[64];
 }
 App_Config;
+
+typedef struct Scene
+{
+	SDL_Color backgroundCol;
+
+	void (*init)();
+	void (*input)(SDL_Event *event);
+	void (*update)();
+	void (*draw)(SDL_Renderer *renderer);
+	void (*cleanup)();
+}
+Scene;
+
+typedef struct GameData
+{
+	Scene scene;
+}
+GameData;
 
 void configure_application(App_Config *app_config);
--- game/src/structs.h
+++ game/src/structs.h
@@ -4,21 +4,4 @@
 
 #include "Core/headers.h"
 
-typedef struct Scene
-{
-	SDL_Color backgroundCol;
-
-	void (*init)();
-	void (*input)(SDL_Event *event);
-	void (*update)();
-	void (*draw)(SDL_Renderer *renderer);
-	void (*cleanup)();
-}
-Scene;
-
-typedef struct GameData
-{
-	Scene scene;
-}
-GameData;
 // TODO: Implement GameData Struct
