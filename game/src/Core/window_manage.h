15346cbaedc91f1bb494e7fb1b1352a6d41b4af5
acerstructor <acerstructorofficial@gmail.com>
2025-07-04 14:44:35 +0800

    added: include header game_config.h

--- game/src/Core/window_manage.h
+++ game/src/Core/window_manage.h
@@ -5,6 +5,7 @@
 #include "../structs.h"
 #include "window_data.h"
 #include "application.h"
+#include "game_config.h"
 
 bool __init_windowData(__window_data* windowData, Game_Config game_config);
 void __draw(__window_data* windowData, GameData* gameData);
