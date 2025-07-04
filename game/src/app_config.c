0ae09c05bc81066f4b4c464dcfec65a2e6934a03
acerstructor <acerstructorofficial@gmail.com>
2025-07-04 14:29:31 +0800

    added statement to initialize first initial scene

--- game/src/app_config.c
+++ game/src/app_config.c
@@ -1,9 +1,11 @@
 #include "Core/entry.h"
+#include "splashscreen.h"
 
 void configure_application(App_Config *app_config)
 {
 	app_config->height = GAME_WINDOW_HEIGHT;
 	app_config->width  = GAME_WINDOW_WIDTH;
+	app_config->initial_scene = init_splashscreen;
 
 	const char* game_window_title = GAME_WINDOW_TITLE;
 
