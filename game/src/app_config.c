201044e67bfb972b7ef16cd95fae183543355ea4
acerstructor <acerstructorofficial@gmail.com>
2025-07-04 14:14:00 +0800

    implemented a function to initialize app_config

--- game/src/app_config.c
+++ game/src/app_config.c
@@ -1,3 +1,11 @@
 #include "Core/entry.h"
 
-// TODO: Application Config
+void configure_application(App_Config *app_config)
+{
+	app_config->height = GAME_WINDOW_HEIGHT;
+	app_config->width  = GAME_WINDOW_WIDTH;
+
+	const char* game_window_title = GAME_WINDOW_TITLE;
+
+	strncpy(app_config->app_title, game_window_title, 64);
+}
